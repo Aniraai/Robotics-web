0681c6bcfb82fc872d10b5d496ec9167be4d75db
Ankit rai <122200322+Aniraai@users.noreply.github.com>
2023-12-19 10:11:14 +0530

    navbar add at 900 px

--- js/script.js
+++ js/script.js
@@ -120,3 +120,17 @@ footerElements.forEach((element) => {
   reverseObserver.observe(element);
 });
 
+//for toogke bar
+function toggleNavbar() {
+  const links = document.querySelector(".linksToggle");
+  links.classList.toggle("show");
+}
+
+document.body.addEventListener("click", function (event) {
+  const links = document.querySelector(".linksToggle");
+  const toggleBtn = document.querySelector(".toggle-btn");
+
+  if (!event.target.closest(".navbar") && event.target !== toggleBtn) {
+    links.classList.remove("show");
+  }
+});
